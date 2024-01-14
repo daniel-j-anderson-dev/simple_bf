ff7c5f44f4dba06f8506fef0596e8fb435faaf0a
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-01-14 01:25:57 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,40 @@
+ # simple_bf
+ This program is a single file bf (brainfuck) interpreter written in rust.
+ - Heap size of 30,000 bytes
+ - Heap values wrap (mod 255)
+ - Heap pointer wraps (mod 30,000)
+
+ # Panics
+ - when I/O operations fail
+ - when arguments are invalid
+ - when bf source code has any `[` or `]` that don't have a match
+
+ # Usage
+ Rust must be installed see: https://www.rust-lang.org/tools/install
+
+ - <b>Default:</b> `simple_bf path/to/source.b`<br>
+    program will halt no later than 1,000 execution cycles
+
+ - <b>Specify halt time:</b> `simple_bf path/to/source.b 906`<br>
+    program will halt after 906 execution cycles
+
+ - <b>Examples:</b> `cd path/to/simple_bf`
+   - `cargo run hello_world.b`
+   - `cargo run echo.b`
+
+# Overview
+ bf consists of
+ - list of values (heap)
+ - value index
+ - list of instructions (program)
+ - instruction index
+
+ There are 8 instructions, any other characters are ignored
+ - `>` increment the value pointer
+ - `<` decrement the value pointer
+ - `+` increment the current value
+ - `-` decrement the current value
+ - `.` write the current value to the standard output stream
+ - `,` store one byte from the standard input stream in the current value
+ - `[` if the current value is 0 jump after the corresponding `]`
+ - `]` if the current value is not 0 jump after the corresponding `[`
---
+++ src/main.rs
@@ -0,0 +1,135 @@
+pub fn main() {
+    let source_code = get_source_code();
+    let cycle_max = get_cycle_max();
+    let program = parse_instructions(&source_code);
+    
+    const HEAP_SIZE: usize = 30000;
+    let cycles_to_complete = execute_bf::<HEAP_SIZE>(&program, cycle_max);
+
+    println!(
+        "\nProgram {} to completion in {} cycles",
+        if cycles_to_complete.is_some() { "executed" } else { "did not execute" },
+        if let Some(index) = cycles_to_complete { index + 1 } else { cycle_max },
+    );
+}
+
+fn execute_bf<const HEAP_SIZE: usize>(program: &[Instruction], cycle_max: usize) -> Option<usize> {
+    let mut heap = [0u8; HEAP_SIZE];
+    let mut pointer = 0;
+    let mut program_counter = 0;
+
+    for cycle_index in 0..cycle_max {
+        if let Some(instruction) = program.get(program_counter) {
+            match instruction {
+                Instruction::IncreasePointer => pointer = (pointer + 1) % HEAP_SIZE,
+                Instruction::DecrementPointer => pointer = pointer.checked_sub(1).unwrap_or(HEAP_SIZE - 1),
+                Instruction::IncreaseValue => heap[pointer] = heap[pointer].wrapping_add(1),
+                Instruction::DecreaseValue => heap[pointer] = heap[pointer].wrapping_sub(1),
+                Instruction::Output => output(heap[pointer]),
+                Instruction::Input => heap[pointer] = input(),
+                Instruction::JumpAhead(jump_index) => {
+                    if heap[pointer] == 0 {
+                        program_counter = *jump_index;
+                    }
+                }
+                Instruction::JumpBack(jump_index) => {
+                    if heap[pointer] != 0 {
+                        program_counter = *jump_index;
+                    }
+                }
+            }
+        } else {
+            return Some(cycle_index);
+        }
+        program_counter += 1;
+    }
+    None
+}
+
+fn parse_instructions(source_code: &str) -> Vec<Instruction> {
+    let mut instructions = Vec::<Instruction>::new();
+    let mut stack = Vec::<usize>::new();
+    for (character_index, character) in source_code
+        .chars()
+        .filter(Instruction::valid_char)
+        .enumerate()
+    {
+        let instruction = match character {
+            '>' => Instruction::IncreasePointer,
+            '<' => Instruction::DecrementPointer,
+            '+' => Instruction::IncreaseValue,
+            '-' => Instruction::DecreaseValue,
+            '.' => Instruction::Output,
+            ',' => Instruction::Input,
+            '[' => {
+                stack.push(character_index);
+                Instruction::JumpAhead(0)
+            }
+            ']' => {
+                let jump_back_index = stack.pop().expect("Missing open bracket(s)");
+                instructions[jump_back_index] = Instruction::JumpAhead(character_index);
+                Instruction::JumpBack(jump_back_index)
+            }
+            _ => continue,
+        };
+
+        instructions.push(instruction);
+    }
+
+    if !stack.is_empty() {
+        panic!("Missing closing bracket(s)");
+    }
+
+    instructions
+}
+
+#[derive(Debug)]
+pub enum Instruction {
+    IncreasePointer,
+    DecrementPointer,
+    IncreaseValue,
+    DecreaseValue,
+    Output,
+    Input,
+    JumpAhead(usize),
+    JumpBack(usize),
+}
+impl Instruction {
+    fn valid_char(character: &char) -> bool {
+        match character {
+            '>' | '<' | '+' | '-' | '.' | ',' | '[' | ']' => true,
+            _ => false,
+        }
+    }
+}
+
+fn output(value: u8) {
+    use std::io::{stdout, Write};
+    stdout().write(&[value]).expect("Failed to write to stdout");
+    stdout().flush().expect("Failed to flush stdout");
+}
+
+fn input() -> u8 {
+    use std::io::{stdin, Read};
+    let mut input = [0];
+    stdin().read(&mut input).expect("Failed to read from stdin");
+    input[0]
+}
+
+fn get_source_code() -> String {
+    std::fs::read_to_string(
+        std::env::args()
+            .nth(1)
+            .expect("Missing source code file path"),
+    )
+    .expect("Failed to read source code file")
+}
+
+fn get_cycle_max() -> usize {
+    const DEFAULT_CYCLE_MAX: &str = "1000";
+    std::env::args()
+        .nth(2)
+        .unwrap_or_else(|| DEFAULT_CYCLE_MAX.to_string())
+        .parse()
+        .expect("First argument is invalid`")
+}
